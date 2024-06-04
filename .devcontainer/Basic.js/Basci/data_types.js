24357f17073611ae31008cd183316b40471f6663
Anisha Rajput <132294490+rajputanisha@users.noreply.github.com>
2024-06-04 22:49:04 +0000

    contianer cofig

---
+++ .devcontainer/Basic.js/Basci/data_types.js
@@ -0,0 +1,16 @@
+"use strict"; // tret sll jd code newer version
+// alert(3+3);// we are nodes js not browser
+let name = "Anisha";
+var age = 18;
+let isLogedd = true;
+let state = null;
+//number ==>2^53
+//bigInt
+//string alwys use double quotes
+//boolean
+//null  standalone value
+//undefined
+//symbol componet unique figma tools ...unique we have symmbol
+//objects
+console.log(typeof undefined);
+// console.log(typeof null); output is object nyll type is object
